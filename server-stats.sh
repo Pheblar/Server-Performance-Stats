dd23961b6e6ccaf2c8610aae9a29082e2e757eef
Pheblar <masterrat511@gmail.com>
2025-01-10 20:29:30 +0200

    feat: memory usage

--- server-stats.sh
+++ server-stats.sh
@@ -5,4 +5,10 @@
      mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 "% used" }'
  }
 
- get_cpu_usage
+get_memory_usage() { 
+    echo "Memory Usage:"
+    free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+}
+
+get_memory_usage
+get_cpu_usage
