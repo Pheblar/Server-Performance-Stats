4f96147866bfabb9036ef2fdace8e2307e7f8ba3
Pheblar <masterrat511@gmail.com>
2025-01-10 21:04:58 +0200

    feat: disk usage add

--- server-stats.sh
+++ server-stats.sh
@@ -1,13 +1,19 @@
  #!/bin/bash
 
- get_cpu_usage() {
-     echo "CPU Usage:"
-     mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 "% used" }'
- }
+get_cpu_usage() {
+    echo "CPU Usage:"
+    mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 "% used" }'
+}
 
 get_memory_usage() { 
     free -m | awk 'NR==2{printf "Memory Usage:\n%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
 }
 
+get_disk_usage() {
+    echo "Disk Usage:"
+    df -h --total | awk 'END{print "Used: "$3", Free: "$4", Usage: "$5}'
+}
+
 get_memory_usage
 get_cpu_usage
+get_disk_usage
